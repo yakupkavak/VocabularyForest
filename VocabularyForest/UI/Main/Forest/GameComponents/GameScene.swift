296880cea886e1b471a05accb54fa681b9724ad1
Yakup Kavak <yakupkavk@gmail.com>
2025-11-19 11:27:13 +0300

    feat: move character right and left

--- VocabularyForest/UI/Main/Forest/GameComponents/GameScene.swift
+++ VocabularyForest/UI/Main/Forest/GameComponents/GameScene.swift
@@ -9,6 +9,12 @@ import SpriteKit
 import SwiftUI
 import AVFoundation
 
+private extension GameScene {
+    enum Constant {
+        static let movingAction = "walkingAnimation"
+    }
+}
+
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
     // MARK: - PROPERTIES
@@ -24,76 +30,86 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var gameTimer: Timer? //control the enemy spawning and movement
     var scoreTimer: Timer? // update every second for the score
 
+    // MARK: - OVERRIDE FUNCTIONS
+    
     override func didMove(to view: SKView) {
         background.color = .backgroundSystem
         physicsWorld.contactDelegate = self // enableds collision dection
         setupBackground()
         setupUI()
         setupTree()
-        setupWalkTextures() // Yeni: Walk.png'den tekstürleri ayır
-        setupPlayer()
+        setupWalkTextures()
         startGame()
     }
+    
+    // MARK: - PRIVATE HELPERS
     
     private func setupWalkTextures() {
         let atlas = SKTextureAtlas(named: "MenWalking")
         var frames: [SKTexture] = []
         var currentIndex = 0
-        
         while currentIndex < atlas.textureNames.count {
             let textureName = "men_walk_\(currentIndex)"
             let texture = atlas.textureNamed(textureName)
             frames.append(texture)
             currentIndex += 1
         }
-        /*
-        let textureName = "men_walk_3"
-        let texture = atlas.textureNamed(textureName)
-        frames.append(texture)
-        let textureName1 = "men_walk_4"
-        let texture1 = atlas.textureNamed(textureName1)
-        frames.append(texture1)
-         */
-        /*
-        currentIndex -= 1
-        
-        while currentIndex > 0 {
-            let textureName = "men_walk_\(i)"
-            let texture = atlas.textureNamed(textureName)
-            frames.append(texture)
-        }
-         */
         walkTextures = frames
+        setupWalkPlayer()
     }
-
-    private func setupPlayer() {
-        guard let firstFrame = walkTextures.first else {
-                print("walkTextures is empty, player cannot be created")
-                return
-            }
-        
-        playerSprite.texture = firstFrame
-        playerSprite.size = firstFrame.size()
-        playerSprite.anchorPoint = CGPoint(x: 0.5, y: 0)
-        playerSprite.position = CGPoint(
-            x: self.size.width / 2,
-            y: floor.size.height * 0.8
-        )
-        
-        playerSprite.zPosition = 3
-        addChild(playerSprite)
-        
-        
+    
+    private func leftMoving() {
+        if playerSprite.action(forKey: Constant.movingAction) != nil {
+            return
+        }
+        playerSprite.xScale = -1.0
         let walkAction = SKAction.animate(
             with: walkTextures,
             timePerFrame: 0.1,
             resize: false,
             restore: false
         )
+        let foreverWalkAction = SKAction.repeatForever(walkAction)
+        let moveLeft = SKAction.moveBy(x: -100, y: 0, duration: 1.0)
+        let foreverWalkMove = SKAction.repeatForever(moveLeft)
+        let walkingAnimationGroup = SKAction.group([foreverWalkAction, foreverWalkMove])
+        playerSprite.run(walkingAnimationGroup, withKey: Constant.movingAction)
+    }
+    
+    private func rightMoving() {
+        if playerSprite.action(forKey: Constant.movingAction) != nil {
+            return
+        }
+        let walkAction = SKAction.animate(
+            with: walkTextures,
+            timePerFrame: 0.1,
+            resize: false,
+            restore: false
+        )
+        playerSprite.xScale = 1.0
+        let foreverWalkAction = SKAction.repeatForever(walkAction)
+        let moveRight = SKAction.moveBy(x: 100, y: 0, duration: 1.0)
+        let foreverWalkMove = SKAction.repeatForever(moveRight)
+        let walkingAnimationGroup = SKAction.group([foreverWalkAction, foreverWalkMove])
+        playerSprite.run(walkingAnimationGroup, withKey: Constant.movingAction)
+    }
+     
+    private func stopMoving() {
+        playerSprite.removeAction(forKey: Constant.movingAction)
+    }
+
+    private func setupWalkPlayer() {
+        guard let firstFrame = walkTextures.first else { return }
         
-        let foreverWalk = SKAction.repeatForever(walkAction)
-        playerSprite.run(foreverWalk, withKey: "walkingAnimation")
-         
+        playerSprite.texture = firstFrame
+        playerSprite.size = firstFrame.size()
+        playerSprite.anchorPoint = CGPoint(x: 0.5, y: 0)
+        playerSprite.position = CGPoint(
+            x: self.size.width / 2,
+            y: floor.size.height * 0.7
+        )
+        playerSprite.zPosition = 3
+        addChild(playerSprite)
     }
     
     private func startGame(){
@@ -151,6 +167,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     func updateScoreLabel(){
         scoreLabel.text = "Timer Surived: \(score) "
+    }
+    
+    // MARK: - TOUCH EVENTS
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let touchLocation = touch.location(in: self)
+        if touchLocation.x > self.size.width / 2 {
+            rightMoving()
+        } else {
+            leftMoving()
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        stopMoving()
     }
 }
 
